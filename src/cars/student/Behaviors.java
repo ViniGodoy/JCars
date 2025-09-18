10133c685fa0daf4913e9ba7853410c19b62aecb
Vinicius Mendonca <Vinicius.Mendonca@wwt.com>
2025-09-18 09:17:39 -0300

    seek, flee, arrive e pursuit

---
+++ src/cars/student/Behaviors.java
@@ -0,0 +1,123 @@
+package cars.student;
+
+import cars.engine.Car;
+import cars.engine.Vector2;
+import cars.engine.World;
+
+import static cars.engine.Vector2.*;
+import static java.lang.Math.acos;
+import static java.lang.Math.toRadians;
+
+/**
+ * Gabarito com todos os steering behaviors implementados
+ */
+public class Behaviors {
+    /**
+     * Move-se em direção ao alvo, a máxima velocidade possível.
+     * @param car Veículo que irá se mover
+     * @param target Alvo.
+     * @return Steering force
+     */
+    public static Vector2 seek(Car car, Vector2 target) {
+        var desiredVelocity = subtract(target, car.getPosition())
+                .resize(car.getMaxSpeed());
+
+        return subtract(desiredVelocity, car.getVelocity());
+    }
+
+    /**
+     * Foge da direção do alvo o mais rápido que puder, desde que este esteja no raio definido pela panicDistance..
+     * @param car Veículo que irá fugir
+     * @param target Alvo.
+     * @param panicDistance Raio em que a fuga ocorre.
+     * @return Steering force
+     */
+    public static Vector2 flee(Car car, Vector2 target, double panicDistance) {
+        var desiredVelocity = subtract(car.getPosition(), target);
+        var size = desiredVelocity.size();
+        if (size > panicDistance) {
+            return vec2();
+        }
+
+        //Não precisamos renormalizar com resize, já que já calculamos size
+        desiredVelocity.multiply(car.getMaxForce() / size);
+        return subtract(desiredVelocity, car.getVelocity());
+    }
+
+    /**
+     * Foge da direção do alvo o mais rápido que puder.
+     * @param car Veículo que irá fugir
+     * @param target Alvo.
+     * @return Steering force
+     *
+     * @see #flee(Car, Vector2, double)
+     */
+    public static Vector2 flee(Car car, Vector2 target) {
+        return flee(car, target, Double.MAX_VALUE);
+    }
+
+    /**
+     * Faz com que o carro desacelere até o alvo
+     * @param car Quem realizará o arrive
+     * @param target Destino
+     * @param deceleration Fator de desaceleração
+     * @param stopDistance Distância em que considerará que chegou.
+     * @return Steering force
+     */
+    public static Vector2 arrive(Car car, Vector2 target, double deceleration, double stopDistance) {
+        var toTarget = subtract(target, car.getPosition());
+        double dist = toTarget.size();
+        if (dist < stopDistance) {
+            return vec2();
+        }
+
+        double speed = Math.min(dist / deceleration, car.getMaxSpeed());
+        var desiredVelocity = toTarget.multiply(speed / dist);
+        return subtract(desiredVelocity, car.getVelocity());
+    }
+
+    /**
+     * Faz com que o carro desacelere até o alvo. Considera stopDistance como 5.
+     * @param car Quem realizará o arrive
+     * @param target Destino
+     * @param deceleration Fator de desaceleração
+     * @return Steering force
+     */
+    public static Vector2 arrive(Car car, Vector2 target, double deceleration) {
+        return arrive(car, target, deceleration, 5);
+    }
+
+    /**
+     * Faz com que o carro desacelere até o alvo. Considera stopDistance=5 e deceleration=1.
+     * @param car Quem realizará o arrive
+     * @param target Destino
+     * @return Steering force
+     */
+    public static Vector2 arrive(Car car, Vector2 target) {
+        return arrive(car, target,1);
+    }
+
+    /**
+     * Tenta interceptar outro veículo, usando para isso sua posição futura.
+
+     * @param pursuer Perseguidor
+     * @param evader Veículo fugitivo
+     * @return A steering force do perseguidor.
+     */
+    public Vector2 pursuit(Car pursuer, Car evader) {
+        var toEvader = subtract(evader.getPosition(), pursuer.getPosition());
+
+        // Se o fugitivo está diretamente a frente vindo em nossa direção
+        // Podemos só fazer um seek para cima dele
+        var isAhead = toEvader.dot(pursuer.getDirection()) > 0;
+        var isFacing = pursuer.getDirection().dot(evader.getDirection()) < acos(toRadians(18));
+        if (isAhead && isFacing) {
+            return seek(pursuer, evader.getPosition());
+        }
+
+        // Se ele está indo para outra direção, estimamos sua posição futura
+        var lookAheadTime = toEvader.size() / (pursuer.getMaxSpeed() * evader.getSpeed());
+        var futurePosition = add(evader.getPosition(), multiply(evader.getVelocity(), lookAheadTime));
+        return seek(pursuer, futurePosition);
+    }
+}
